d76503c4fe73d184634a53546998c42ade43aa1d
Emma Harper Smith <emma@emmatyping.dev>
2025-09-04 20:18:34 -0700

    Update a bunch of dependencies etc.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,7 @@
 	<div class="m-20 text-2xl">
 		mypy.app is a playground for experimenting with typing and mypy. It was created by <a
 			class="text-slate-800 underline"
-			href="https://twitter.com/ethanhs">Ethan Smith</a
+			href="https://bsky.app/profile/emmatyping.dev">Emma Smith</a
 		>. This project would likely have taken a lot longer without the work of the
 		<a class="text-slate-800 underline" href="https://pyodide.org">pyodide team</a>, many many
 		thanks to them! The project was inspired by
@@ -12,7 +12,7 @@
 		> I also would like to thank my friends for trying the app and giving valuable feedback.
 	</div>
 	<div class="flex items-center justify-center m-10">
-		<a class="text-slate-800 underline text-2xl" href="https://github.com/ethanhs/mypy.app">
+		<a class="text-slate-800 underline text-2xl" href="https://github.com/emmatyping/mypy.app">
 			Github link
 		</a>
 	</div>
