ede38f1352e7465a690bfed1673f58cac5a8d1c7
Emma Harper Smith <emma@emmatyping.dev>
2025-12-17 04:53:52 +0000

    Upgrade to sveltekit2, svelte 4

--- src/routes/short/+server.ts
+++ src/routes/short/+server.ts
@@ -7,7 +7,7 @@ export async function GET(event: RequestEvent) {
 	const params = event.url.searchParams;
 	const short = params.get('from');
 	if (short === null) {
-		throw error(400, 'from must be set');
+		error(400, 'from must be set');
 	} else {
 		const long = await shortener.unshorten(short);
 		return new Response(long);
@@ -17,7 +17,7 @@ export async function GET(event: RequestEvent) {
 export async function POST(event: RequestEvent) {
 	let value = await event.request.text();
 	if (value.length > 5 * 10 ** 6) {
-		throw error(413, 'Input Python code too long, sorry!');
+		error(413, 'Input Python code too long, sorry!');
 	}
 	return new Response(await shortener.shorten(value));
 }
